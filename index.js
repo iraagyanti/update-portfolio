05793b45045ef199b1b961457750dcabb73652b4
Ira <73374826+iraagyanti@users.noreply.github.com>
2021-01-29 15:53:58 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// Navigation Menu
+function openNav() {
+  document.getElementById("myNavbar").style.width = "100%";
+}
+function closeNav() {
+  document.getElementById("myNavbar").style.width = "0";
+}
